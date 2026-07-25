5375af6a48e65afc483f71e93732da2d6458c9cc
Andrii <andrii@a2g.name>
2026-07-25 21:09:33 +0300

    fixed verifying script by handling errors

--- scripts/verify.ps1
+++ scripts/verify.ps1
@@ -1,5 +1,8 @@
 $ErrorActionPreference = "Stop"
 
 cargo fmt --all -- --check
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 cargo clippy --all-targets --all-features -- -D warnings
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 cargo test --all-targets --all-features
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
